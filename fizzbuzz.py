84f8cd8c967ea4336e1ba36c6478f734331fea7d
Shota Fubasami <shotafubasami@iMac.local>
2018-06-28 13:34:12 +0900

    add new file

---
+++ fizzbuzz.py
@@ -0,0 +1,12 @@
+# test git
+# fizzbuzz
+N = 100
+for i in range(1,N):
+    if i%15 == 0:
+        print('Fizz Buzz')
+    elif i%5 == 0:
+        print('Buzz')
+    elif i%3 == 0:
+        print('Fizz')
+    else:
+        print(i)
